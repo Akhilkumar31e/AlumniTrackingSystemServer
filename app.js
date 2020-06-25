01643479045be1b25321c42e853a3e7296400938
Akhilkumar31e <akhilkumar31e@gmail.com>
2020-06-25 13:10:35 +0530

    User authentication Done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 //Database mongoose connection
 const mongoose = require('mongoose');
@@ -27,10 +29,40 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser());
+app.use((session({
+  name:'session-id',
+  secret:'12345-67890-09876-54321',
+  saveUninitialized:false,
+  resave:false,
+  store: new FileStore()
+})));
+
 
 app.use('/', indexRouter);
+function auth(req,res,next) {
+  console.log(req.session);
+  if(!req.session.user){
+    var err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err); 
+  }
+  else{
+    if(req.session.user==='authenticated') {
+      next();
+    }
+    else{
+      var err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err); 
+    }
+  }
+}
+//User authentication
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,90 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/signup',(req,res,next) => {
+  User.findOne({rollnumber: req.body.rollnumber})
+  .then((user) => {
+    if( user!=null ) {
+      var err=new Error('User ' +req.body.rollnumber+' already exists');
+      err.status = 403;
+      next(err);
+    }
+    else{
+      return User.create(req.body);
+    }
+  })
+  .then((user) => {
+    res.statusCode=200;
+    res.setHeader('Content-Type','application/json');
+    res.json({status:'Registration Successful', user:user});
+
+  },(err)=> next(err))
+  .catch((err) => next(err));
+});
+
+router.post('/login',(req,res,next) => {
+  if(!req.session.user){
+    var authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+      var err = new Error('You are not authenticated');
+  
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err); 
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+  
+    var username = auth[0];
+    var password = auth[1];
+    User.findOne({rollnumber: username})
+    .then((user) => {
+        if(user===null){
+          var err = new Error('User '+ username + 'doesnt exist');
+          err.status = 403;
+          return next(err); 
+        }
+        else if(user.password !== password) {
+          var err = new Error('Password '+ password + 'is incorrect');
+          err.status = 403;
+          return next(err); 
+        }
+        else if(user.rollnumber===username && user.password === password){
+          req.session.user = 'authenticated';
+          res.statusCode= 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('You are now logged in');
+        }
+    })
+    .catch((err)=> next(err));
+  }
+  else{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('You are already logged in');
+  }
+});
+
+router.get('/logout',(req,res) => {
+  if(req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else{
+    var err= new Error('You are not logged in');
+    err.status = 403;
+    return next(err);
+  }
+});
+
+
 
 module.exports = router;
